3f0be021645ba4c945990cd92a4d2d9867675717
yuze <looker53@sina.com>
2021-05-21 18:07:11 +0800

    封装请求类,保存url,method,headers

--- server.py
+++ server.py
@@ -8,6 +8,37 @@ import typing
 
 HOST = '127.0.0.1'
 PORT = 9000
+# 每行用 \r\n 分割，content-length 长度确认
+RESPONSE = b"""\
+HTTP/1.1 200 OK
+Content-type: text/html
+Content-length: 18
+
+<h1>Hello Yuz!</h1>""".replace(b'\n', b'\r\n')
+
+
+
+class Request(typing.NamedTuple):
+    """请求类"""
+    path: str
+    method: str
+    headers: typing.Mapping
+
+    @classmethod
+    def from_socket(cls, sock: socket.socket) -> 'Request':
+        lines = iter_request_lines(sock)
+        try:
+            request_line = next(lines).decode()
+        except StopIteration:
+            raise ValueError('no request line')
+
+        method, path, _ = request_line.split(' ')
+
+        headers = {}
+        for line in lines:
+            key, _, value = line.decode().partition(':')
+            headers[key.lower()] = value.lstrip()
+        return cls(path=path, method=method, headers=headers)
 
 
 def iter_request_lines(sock: socket.socket, buff_size=1024):
@@ -27,14 +58,6 @@ def iter_request_lines(sock: socket.socket, buff_size=1024):
                 return buff
 
 
-# 每行用 \r\n 分割，content-length 长度确认
-RESPONSE = b"""\
-HTTP/1.1 200 OK
-Content-type: text/html
-Content-length: 18
-
-<h1>Hello Yuz!</h1>""".replace(b'\n', b'\r\n')
-
 with socket.socket() as sock:
     sock.bind((HOST, PORT))
     # 0怎么确定的? 学习 socket 协议簇
@@ -42,8 +65,7 @@ with socket.socket() as sock:
     print(f"listen on {HOST}:{PORT}")
     while True:
         conn, address = sock.accept()
+        request = Request.from_socket(conn)
         print(f"new connection from {address}")
-        with conn:
-            for line in iter_request_lines(conn):
-                print(line)
-            conn.sendall(RESPONSE)
+        print(f"请求数据:{request}")
+        conn.sendall(RESPONSE)
