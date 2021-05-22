ab1f6bec7737383e9d106994a2015928be558b8e
yuze <looker53@sina.com>
2021-05-22 14:59:56 +0800

    静态文件响应

--- server.py
+++ server.py
@@ -5,9 +5,12 @@
 # -----------------------------------------------
 import socket
 import typing
+import pathlib
+import mimetypes
 
 HOST = '127.0.0.1'
 PORT = 9000
+STATIC_PATH = 'static'
 # 每行用 \r\n 分割，content-length 长度确认
 RESPONSE = b"""\
 HTTP/1.1 200 OK
@@ -15,6 +18,14 @@ Content-type: text/html
 Content-length: 18
 
 <h1>Hello Yuz!</h1>""".replace(b'\n', b'\r\n')
+
+# 静态文件请求模板
+FILE_RESPONSE_TEMPLATE = """\
+HTTP/1.1 200 OK
+Content-type: {content_type}
+Content-length: {content_length}
+
+""".replace('\n', '\r\n')
 
 # bad request 响应结果
 BAD_REQUEST_RESPONSE = b"""\
@@ -24,7 +35,7 @@ Content-length: 11
 
 Bad Request""".replace(b"\n", b"\r\n")
 
-# not foound 响应结果
+# not found 响应结果
 NOT_FOUND_RESPONSE = b"""\
 HTTP/1.1 404 Not Found
 Content-type: text/plain
@@ -77,6 +88,28 @@ def iter_request_lines(sock: socket.socket, buff_size=1024):
                 return buff
 
 
+def send_file(sock: socket.socket, path: str) -> None:
+    """发送静态文件给客户端"""
+    if path == '/':
+        path = '/index.html'
+    file_path = pathlib.Path(STATIC_PATH) / path.lstrip('/')
+    if not file_path.exists():
+        sock.sendall(NOT_FOUND_RESPONSE)
+        return
+
+    content_type, encoding = mimetypes.guess_type(file_path)
+    if content_type is None:
+        content_type = 'application/octet-stream'
+    if encoding is not None:
+        content_type += f'; charset={encoding}'
+    size = file_path.stat().st_size
+    headers = FILE_RESPONSE_TEMPLATE.format(content_type=content_type,
+                                            content_length=size)
+    sock.sendall(headers.encode('utf-8'))
+    with file_path.open('rb', encoding=encoding) as f:
+        sock.sendfile(f)
+
+
 with socket.socket() as sock:
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((HOST, PORT))
@@ -89,6 +122,7 @@ with socket.socket() as sock:
         try:
             request = Request.from_socket(conn)
             print(f"请求数据:{request}")
-            conn.sendall(NOT_FOUND_RESPONSE)
+            # conn.sendall(NOT_FOUND_RESPONSE)
+            send_file(conn, request.path)
         except BadRequestException:
             conn.sendall(BAD_REQUEST_RESPONSE)
